82091bab686600a3962e4ca227b4d3906e9f8051
Sema <logan22f@gmail.com>
2023-04-22 01:16:08 +0300

    sql for ticket

---
+++ install/sql/struct/tickets.sql
@@ -0,0 +1,19 @@
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tickets
+-- ----------------------------
+DROP TABLE IF EXISTS `tickets`;
+CREATE TABLE `tickets`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NULL DEFAULT NULL,
+  `content` varchar(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp,
+  `close` int NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE,
+  FULLTEXT INDEX `content_index`(`content`)
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
