37dd5c33af108cdb185aeed4ab65ddc51e7d57c5
Ofey <logan22f@gmail.com>
2023-06-11 18:09:49 +0300

    Доработка инсталяции

--- src/component/version/version.php
+++ src/component/version/version.php
@@ -9,9 +9,9 @@ namespace Ofey\Logan22\component\version;
 
 class version {
 
-    private const MIN_PHP_VERSION = 8.1;
+    private const MIN_PHP_VERSION = 8.2;
 
-    static public function check_version_php() : void {
+    public static function check_version_php() : void {
         if ((float) PHP_VERSION < self::MIN_PHP_VERSION()) {
             echo sprintf("Need min version php : %.1f<br>", self::MIN_PHP_VERSION());
             echo 'Your php version : ' . PHP_VERSION;
@@ -20,11 +20,11 @@ class version {
         self::libsPHP();
     }
 
-    static public function MIN_PHP_VERSION() : float {
+    public static function MIN_PHP_VERSION() : float {
         return self::MIN_PHP_VERSION;
     }
 
-    static public function libsPHP() : void {
+    public static function libsPHP() : void {
         if (!extension_loaded('gd') && !function_exists('gd_info')) {
             echo 'Need to enable GD in php.ini';
             exit;
