3b93943fb334b02906c12593609839c2812242ce
Ofey <logan22f@gmail.com>
2023-07-20 17:52:11 +0300

    upd server_list

--- uploads/sql/struct/server_list.sql
+++ uploads/sql/struct/server_list.sql
@@ -1,6 +1,9 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+-- ----------------------------
+-- Table structure for server_list
+-- ----------------------------
 DROP TABLE IF EXISTS `server_list`;
 CREATE TABLE `server_list`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -29,7 +32,11 @@ CREATE TABLE `server_list`  (
   `chat_game_enabled` int(11) NULL DEFAULT 0,
   `launcher_enabled` int(11) NULL DEFAULT 0,
   `timezone` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `rest_api_enable` int(11) NOT NULL DEFAULT 0,
+  `rest_api_hostname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `rest_api_port` int(11) NULL DEFAULT NULL,
+  `rest_api_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
