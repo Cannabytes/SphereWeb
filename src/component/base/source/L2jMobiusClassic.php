c49b3387654cbbe0d4ba2a11631bf568b4d49e90
Ofey <logan22f@gmail.com>
2023-11-17 10:14:19 +0200

    l2jMobuis fix show lvl/race sex

--- src/component/base/source/L2jMobiusClassic.php
+++ src/component/base/source/L2jMobiusClassic.php
@@ -268,6 +268,7 @@ class L2jMobiusClassic implements structure {
                         characters.pvpkills AS pvp,
                         characters.pkkills AS pk,
                         characters.`online`,
+                        characters.`sex`,
                         characters.onlinetime AS time_in_game,
                         clan_data.clan_name,
                         clan_data.clan_level,
@@ -403,20 +404,27 @@ ORDER BY
 
     static public function account_players(): string {
         return 'SELECT
-            characters.account_name, 
-            characters.charId AS player_id, 
-            characters.char_name AS player_name, 
-            characters.pvpkills AS pvp, 
-            characters.pkkills AS pk, 
-              characters.title,
-              characters.sex,
-            characters.clanid, 
-            characters.classid, 
-            characters.base_class AS class_id, 
-            characters.`online`, 
-            characters.onlinetime AS time_in_game
-        FROM
-            characters
-        WHERE  characters.account_name = ?;';
+                        characters.account_name AS account_name,
+                        characters.charId AS player_id,
+                        characters.char_name AS player_name,
+                        characters.sex,
+                        characters.x,
+                        characters.y,
+                        characters.z,
+                        characters.pvpkills AS pvp,
+                        characters.pkkills AS pk,
+                        characters.clanid AS clan_id,
+                        characters.`online`,
+                        characters.onlinetime AS time_in_game,
+                        clan_data.clan_name,
+                        characters.`level`,
+                        characters.`classid` AS class_id,
+                        crests.`data` AS clan_crest,
+                       ( SELECT `data` FROM crests WHERE crest_id = clan_data.ally_crest_id LIMIT 1) AS alliance_crest 
+                    FROM
+                        characters
+                        LEFT JOIN clan_data ON characters.clanid = clan_data.clan_id
+                        LEFT JOIN crests   ON clan_data.crest_id = crests.crest_id
+                        WHERE characters.account_name = ?;';
     }
 }
