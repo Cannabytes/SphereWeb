c3c6d99daa0f72b110dc3dc41e29272033457cd9
Ofey <logan22f@gmail.com>
2023-12-16 07:01:28 +0200

    Фикс ошибки отсутствия персонажа

--- src/component/plugins/launcher/launcher.php
+++ src/component/plugins/launcher/launcher.php
@@ -20,28 +20,34 @@ class launcher {
         $show_accounts = $config['server'][1]['show_accounts'] ?? false;
         tpl::addVar('show_accounts', $show_accounts);
 
-        if($show_accounts){
+        if ($show_accounts) {
             $player_accounts = player_account::show_all_account_player();
             $accounts = [];
-            if($player_accounts){
+
+            if (!empty($player_accounts)) {
                 foreach ($player_accounts as &$account) {
                     $login = $account['login'];
                     $password = $account['password'];
                     $players = character::all_characters($login);
+
                     if (!$players) {
                         continue;
                     }
-                    if (sdb::is_error()){
+
+                    if (sdb::is_error()) {
                         return null;
                     }
+
                     foreach ($players as $player) {
                         $accounts[$login]["players"][] = $player["player_name"];
                         $accounts[$login]['password'] = $password;
                     }
                 }
             }
+
             tpl::addVar('accounts_list', $accounts);
         }
+
 
         tpl::addVar('userLang', lang::lang_user_default());
         tpl::addVar("code", base64_encode(json_encode($config['server'][1])));
