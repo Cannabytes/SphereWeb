7c90d3ee82e4b9ec90b5c69a82a9519753b50c6d
Ofey <logan22f@gmail.com>
2023-12-24 11:36:24 +0200

    улучшение определение https/http на случай не настоенного сервера

--- src/template/tpl.php
+++ src/template/tpl.php
@@ -164,10 +164,14 @@ class tpl {
 
 
         self::$allTplVars['dir'] = fileSys::localdir();
-        $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https' : 'http';
-        if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+        $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https'? 'https' : 'http';
+        if($_SERVER["SERVER_PORT"] == 443)
             $protocol = 'https';
-        }
+        elseif (isset($_SERVER['HTTPS']) && (($_SERVER['HTTPS'] == 'on') || ($_SERVER['HTTPS'] == '1')))
+            $protocol = 'https';
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on')
+            $protocol = 'https';
+
         $self = $protocol . "://" . $_SERVER["SERVER_NAME"] . $relativePath . self::$templatePath;
 
         self::$allTplVars['protocol'] = $protocol;
